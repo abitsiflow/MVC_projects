78f056b9ad066cdad808969af8a76b1fd5e2b104
abitsiflow <xtiangzmen@gmail.com>
2026-08-30 11:53:12 +0800

    initial commit

---
+++ mimoMVC/Controllers/myController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace mimoMVC.Controllers
+{
+    public class myController : Controller
+    {
+        public ActionResult myView()
+        {
+            ViewBag.Message = "This is my view page, What's up? :)";
+            return View();
+        }
+    }
+}
